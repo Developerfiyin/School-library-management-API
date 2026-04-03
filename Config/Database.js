4976cc602321db4ed58ae5466c4d105b297f7b3c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-03 21:46:26 +0100

    Update authorController.js

--- Config/Database.js
+++ Config/Database.js
@@ -3,7 +3,7 @@ const URL = process.env.MONGODB_URI;
 
 async function connectDB ( ) {
     await mongoose.connect(URL);
-    console.log("Davsal is here !!!!!");
+    console.log("Davsal is here with me on a whatsapp call !!!!!");
     
 
 }
