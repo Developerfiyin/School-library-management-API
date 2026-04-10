c293dd7b38eda2b71fcbf5d133bc4bee3f59d59f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-10 15:04:58 +0100

    Update book controller.js

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -1,2 +1,21 @@
 import books from "../Models/booksModel.js";
 
+exports.borrowedBooks = async (req, res) => {
+    try {
+   const {returnDate, StudentId, LibrarianId} = req.body;
+   const bookId = req.params.id;
+    const book = await books.findById(bookId);
+
+    if(!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+
+    if (book.Status === "OUT") {
+        return res.status(400).json ({message: "OOPs!!, the book out  already!. Please try again later"});
+    }
+    }
+    catch (error) {
+
+    }
+}
