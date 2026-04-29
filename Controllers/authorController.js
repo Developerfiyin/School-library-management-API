f8018ae0aebf06c67d7f878f99cea7c5467475d8
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-04-29 15:46:46 +0100

    Create the logic for getting all authors and getting them by an id

--- Controllers/authorController.js
+++ Controllers/authorController.js
@@ -14,11 +14,25 @@ export const createAuthor = async (req, res) => {
 
 //GET ALL AUTHORS
 
-export const getAuthors = () => {
+export const getAuthors = async (req, res) => {
  try {
-    const authors = await author
+    const authors = await authorSchema.find().sort({ name: 1 })
+    res.status(200).json({ ok: true, message: "Request successful", data: authors })
  } catch (error) {
-    
+    res.status(500).json({ ok: false, message: "Error fetching authors" })
  }
 }
 
+//GET AUTHOR BY ID
+export const getAuthorById = async (req, res) => {
+    const id = req.params.id
+    try {
+        const author = await authorSchema.findById(id)
+        if (!author) {
+            return res.status(400).json({ ok: false, message: "Invalid Input!! Fill the field correctly", data: null })
+        }
+        res.status(200).json({ ok: true, message: "Request successful", data: author })
+    } catch (error) {
+        res.status(500).json({ ok: false, message: "Error fetching author" })
+    }
+}
