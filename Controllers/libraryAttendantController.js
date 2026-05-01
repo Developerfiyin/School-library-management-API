fd45f4b85a783ffaee35b121a40630c6711b0371
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-05-01 10:06:04 +0100

    Create logic for getting library attendant

--- Controllers/libraryAttendantController.js
+++ Controllers/libraryAttendantController.js
@@ -5,9 +5,9 @@ import Library from "../Models/libraryModel";
 //CREATE LIBRARY ATTENDANT
 
 export const createLibraryAttendant = async (req, res) => {
-  const { name } = req.body;
+
   try {
-    const libraryAttendant = await Library.create({ name });
+    const libraryAttendant = await Library.create( req.body);
 
     res
       .status(201)
@@ -22,3 +22,15 @@ export const createLibraryAttendant = async (req, res) => {
       .json({ ok: false, message: "Error creating library attendant" });    
   }
 };
+
+//GET ALL LIBRARY ATTENDANTS
+
+export const getLibraryAttendants = async (req, res) => {
+    try {
+        const libraryAttendants = await Library.find().sort({ name: 1 })
+        res.status(200).json({ ok: true, message: "Request successful", data: libraryAttendants })
+        } catch (error) {
+        res.status(500).json({ ok: false, message: "Error fetching library attendants" })
+    }
+} 
+
