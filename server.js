0c83570af645a1dc87f87fc977f662a185448ad3
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-05-12 20:18:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,39 +3,23 @@ import "dotenv/config";
 
 import connectDB from "./Config/Database.js";
 
-
- const app =  express();
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World, welcome to the School Library Management API!");
 });
-const startDb = async () => {
- 
+
 try {
- 
-
-  app.listen(3000, () => {
-   console.log("Server is running on http://localhost:3000");
-
-
-});
-
-console.log("Jude is here!!!!!!!!!")
- await connectDB();
-
+  const startDb = async () => {
+    console.log("Jude is here!!!!!!!!!");
+    await connectDB();
+  };
 } catch (error) {
-    console.error("Failed to connect to the database:", error);
-}
- 
- 
-
-
+  console.error("Failed to connect to the database:", error);
 }
 
+startDb();
 
-
-
- startDb();
-
-
-
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
